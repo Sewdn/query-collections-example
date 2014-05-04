4659f41e2e858859a9bff78166143219bcb23c42
Sewdn <pieter.soudan@redandivory.be>
2014-05-04 14:44:25 +0200

    add observers

--- client/ranking3.js
+++ client/ranking3.js
@@ -1,17 +1,36 @@
 Template.ranking3.helpers({
   'ranking': function(){
-    return GameRanking2.find(
-      {}
-    );
+    return query;
   },
   'unique': function(){
     return Random.id().substr(0,3);
   }
 });
 
+var query;
+
 Template.ranking3.created = function() {
   $(".ranking").on("webkitTransitionEnd", function () {
     $(this).hide().offset();
     $(this).show();
+  });
+  query = GameRanking2.find(
+    {}
+  );
+  query.observeChanges({
+    added: function (id, fields) {
+      console.log('added', fields);
+      // ...
+    },
+    changed: function (id, fields) {
+      console.log('changed', fields);
+      // ...
+    },
+    movedBefore: function (id, fields) {
+      // ...
+    },
+    removed: function (id) {
+      // ...
+    }
   });
 };
